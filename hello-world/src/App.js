228d0d59ac4c6ea341149a951f3822e4cc6df6c2
FahadShaik <shaikfahad904@gmail.com>
2024-02-28 21:35:51 +0530

    Navbar component

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import Navigation from "./components/Navigation";
+import HelloWorld from "./components/NewHelloWorld";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/components/NewHelloWorld.js
@@ -0,0 +1,10 @@
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p> This is my first create react app</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
