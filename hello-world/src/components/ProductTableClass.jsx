a64d5372f6829cf75a8340add8095d3d16b023d0
FahadShaik <shaikfahad904@gmail.com>
2024-03-11 15:18:11 +0530

    State in class with spread operator

--- hello-world/src/components/ProductTableClass.jsx
+++ hello-world/src/components/ProductTableClass.jsx
@@ -2,21 +2,34 @@ import React from "react";
 
 class ProductTableClass extends React.Component {
   state = {
-    name: "Apple iPhone 15 (Blue, 128 GB)",
-    price: 66499,
-    imageUrl:
-      "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/l/l/-original-imagtc5fz9spysyk.jpeg?q=70",
-    qty: 1,
+    product: {
+      name: "Apple iPhone 15 (Blue, 128 GB)",
+      price: 66499,
+      imageUrl:
+        "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/l/l/-original-imagtc5fz9spysyk.jpeg?q=70",
+      qty: 1,
+    },
   };
 
   incrementHandler = () => {
-    this.setState({ qty: this.state.qty + 1 });
+    this.setState({
+      product: {
+        ...this.state.product,
+        qty: this.state.product.qty + 1,
+      },
+    });
   };
   decrementHandler = () => {
-    this.setState({ qty: this.state.qty - 1 });
+    this.setState({
+      product: {
+        ...this.state.product,
+        qty: this.state.product.qty - 1,
+      },
+    });
   };
 
   render() {
+    console.log(this.state.product);
     return (
       <>
         <h2>Product Details</h2>
@@ -24,35 +37,39 @@ class ProductTableClass extends React.Component {
           <div className="row">
             <div className="col-md-10">
               <table className="table">
-                <thead>
-                  <th>Product Name</th>
-                  <th>Image</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Total</th>
+                <thead className="bg-dark text-white">
+                  <tr>
+                    <th>Product Name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                  </tr>
                 </thead>
                 <tbody>
-                  <td>{this.state.name}</td>
-                  <td>
-                    <img
-                      src={this.state.imageUrl}
-                      width={"100px"}
-                      alt="product_image"
-                    />
-                  </td>
-                  <td>{this.state.price}</td>
-                  <td>
-                    <i
-                      className="fa fa-minus-circle"
-                      onClick={this.decrementHandler}
-                    ></i>
-                    {this.state.qty}
-                    <i
-                      className="fa fa-plus-circle"
-                      onClick={this.incrementHandler}
-                    ></i>
-                  </td>
-                  <td>{this.state.qty * this.state.price}</td>
+                  <tr>
+                    <td>{this.state.product.name}</td>
+                    <td>
+                      <img
+                        src={this.state.product.imageUrl}
+                        width={"100px"}
+                        alt="product_image"
+                      />
+                    </td>
+                    <td>{this.state.product.price}</td>
+                    <td>
+                      <i
+                        className="fa fa-minus-circle"
+                        onClick={this.decrementHandler}
+                      ></i>
+                      {this.state.product.qty}
+                      <i
+                        className="fa fa-plus-circle"
+                        onClick={this.incrementHandler}
+                      ></i>
+                    </td>
+                    <td>{this.state.product.price * this.state.product.qty}</td>
+                  </tr>
                 </tbody>
               </table>
             </div>
